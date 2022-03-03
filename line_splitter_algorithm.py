deeb06e28856989910d7c3c90a94a2fb20d375b7
Artsiom Sauchuk <artemsavchuk01@gmail.com>
2022-03-03 19:32:27 +0100

    Daily

--- line_splitter_algorithm.py
+++ line_splitter_algorithm.py
@@ -33,11 +33,23 @@ __revision__ = '$Format:%H$'
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
+                       QgsVectorLayer,
+                       QgsGeometry,
+                       QgsFeature,
+                       QgsField,
+                       QgsFields,
+                       QgsCoordinateTransformContext,
+                       QgsVectorFileWriter,
                        QgsProcessingAlgorithm,
                        QgsProcessingParameterFeatureSource,
                        QgsProcessingParameterFeatureSink)
 from qgis import processing
+from qgis.PyQt.QtCore import QVariant
+import pdb
 
+def from_point_list(points_features):
+    for f in points_features:
+        point = f.geometry().asPoint()
 
 class LineSplitterAlgorithm(QgsProcessingAlgorithm):
     """
@@ -58,7 +70,7 @@ class LineSplitterAlgorithm(QgsProcessingAlgorithm):
     # calling from the QGIS console.
 
     OUTPUT = 'OUTPUT'
-    INPUT = 'INPUT'
+    INPUT_1 = 'INPUT'
     INPUT_POINTS = 'INPUT_POINTS'
 
     def initAlgorithm(self, config):
@@ -71,7 +83,7 @@ class LineSplitterAlgorithm(QgsProcessingAlgorithm):
         # geometry.
         self.addParameter(
             QgsProcessingParameterFeatureSource(
-                self.INPUT,
+                self.INPUT_1,
                 self.tr('Input layer'),
                 [QgsProcessing.TypeVectorLine]
             )
@@ -103,47 +115,120 @@ class LineSplitterAlgorithm(QgsProcessingAlgorithm):
         # Retrieve the feature source and sink. The 'dest_id' variable is used
         # to uniquely identify the feature sink, and must be included in the
         # dictionary returned by the processAlgorithm function.
-        source = self.parameterAsSource(parameters, self.INPUT, context)
+        source = self.parameterAsSource(parameters, self.INPUT_1, context)
         points_splitter_layer = self.parameterAsSource(parameters, self.INPUT_POINTS, context)
         
+        output_fields = QgsFields()
+        output_fields.append(QgsField("sec_id", QVariant.Int))
         (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT,
-                context, source.fields(), source.wkbType(), source.sourceCrs())
+                context, output_fields, source.wkbType(), source.sourceCrs())
         
         
-        line_point_layer = processing.run("native:extractvertices", {'INPUT': source})
-        snapped_layer = processing.run("native:snapgeometries", {'INPUT': points_splitter_layer, 'REFERENCE_LAYER': source, 'TOLERANCE': 100, 'BEHAVIOR': 1})
-        joined_layer = processing.run("native:joinbynearest", {'INPUT':line_point_layer,'INPUT_2':snapped_layer,'FIELDS_TO_COPY':['ord_ass'],'DISCARD_NONMATCHING':False,'PREFIX':'PATH_','NEIGHBORS':2,'MAX_DISTANCE':None,'OUTPUT':'TEMPORARY_OUTPUT'})
+        line_point_layer = (processing.run(
+            "native:extractvertices", 
+            {
+                'INPUT': '/home/artem/Work_general/ASTRAL_maps/csv/path.gpkg|layername=path', 
+                'OUTPUT':'/home/artem/vertices.'
+            },
+            is_child_algorithm=True))['OUTPUT']
+        pdb.set_trace()
+        snapped_layer = (processing.run(
+            "native:snapgeometries", 
+            {
+                'INPUT': points_splitter_layer.sourceName(), 
+                'REFERENCE_LAYER': source.sourceName(), 
+                'TOLERANCE': 100, 
+                'BEHAVIOR': 1, 
+                'OUTPUT':'TEMPORARY_OUTPUT'
+            },
+            context=context, 
+            feedback=feedback, 
+            is_child_algorithm=True))['OUTPUT']
+        
+        
+        joined_layer = (processing.run(
+            "native:joinbynearest",
+            {
+                'INPUT':line_point_layer,
+                'INPUT_2':snapped_layer,
+                'FIELDS_TO_COPY':['ord_ass'],
+                'DISCARD_NONMATCHING':False,
+                'PREFIX':'PATH_',
+                'NEIGHBORS':2,
+                'MAX_DISTANCE':None,
+                'OUTPUT':'TEMPORARY_OUTPUT'
+            },
+            context=context, 
+            feedback=feedback, 
+            is_child_algorithm=True))['OUTPUT']
 
+
+       
+        line_point_layer = QgsVectorLayer(line_point_layer)
+        joined_layer = QgsVectorLayer(joined_layer)
+        pdb.set_trace()
+
+        # ----------------------------------
+        # path = "/home/artem/File.shp"    
+        # options = QgsVectorFileWriter.SaveVectorOptions()
+        # options.driverName = "ESRI Shapefile"
+
+        # QgsVectorFileWriter.writeAsVectorFormatV2(line_point_layer, path, QgsCoordinateTransformContext(), options)
+        # ----------------------------------
+
+        
+        # pdb.set_trace()
         line_point_features = line_point_layer.getFeatures()
         line_point_f_list = list()
         # sort line_point_features by ord_ass
         for feature in line_point_features:
+            
             line_point_f_list.append(feature)
         line_point_f_list.sort(key=lambda f: int(f['ord_ass']))
 
-        joined_m = dict()
-        for feature in line_point_features:
+        joined_d = dict()
+        for feature in joined_layer.getFeatures():
+            
             id = feature.id()
-            if id not in joined_m or (id in joined_m and joined_m[id]["PATH_ord_ass"] > feature["PATH_ord_ass"]):
-                joined_m[id] = feature
-        
+            if id not in joined_d or (id in joined_d and joined_d[id]["PATH_ord_ass"] > feature["PATH_ord_ass"]):
+                joined_d[id] = feature
 
+        invrt_joined = dict()       
+    
+        for f in joined_d.values():
+            invrt_joined[f["PATH_ord_ass"]] = f
+        merged_l = list()
+        merged_l.append(list())
+        for line_f in line_point_f_list:
+            merged_l[-1].append(line_f)
+            if line_f["ord_ass"] in invrt_joined:
+                merged_l[-1].append(invrt_joined[line_f["ord_ass"]])
+                merged_l.append(list())
+                merged_l[-1].append(invrt_joined[line_f["ord_ass"]])
 
+        for i, l in enumerate(merged_l):
+            fet = QgsFeature(output_fields)
+            fet.setAttribute('sec_id', i)
+            fet.setGeometry(QgsGeometry.fromPolylineXY([f.geometry().asPoint() for f in l]))
+            sink.addFeature(fet, QgsFeatureSink.FastInsert)
         # Compute the number of steps to display within the progress bar and
         # get features from source
-        total = 100.0 / source.featureCount() if source.featureCount() else 0
-        features = source.getFeatures()
+        
+        
+        # total = 100.0 / source.featureCount() if source.featureCount() else 0
+        # features = source.getFeatures()
 
-        for current, feature in enumerate(features):
-            # Stop the algorithm if cancel button has been clicked
-            if feedback.isCanceled():
-                break
+        # for current, feature in enumerate(features):
+        #     # Stop the algorithm if cancel button has been clicked
+        #     if feedback.isCanceled():
+        #         break
 
-            # Add a feature in the sink
-            sink.addFeature(feature, QgsFeatureSink.FastInsert)
+        #     # Add a feature in the sink
+        #     sink.addFeature(feature, QgsFeatureSink.FastInsert)
 
-            # Update the progress bar
-            feedback.setProgress(int(current * total))
+        #     # Update the progress bar
+        #     feedback.setProgress(int(current * total))
+
 
         # Return the results of the algorithm. In this case our only result is
         # the feature sink which contains the processed features, but some
